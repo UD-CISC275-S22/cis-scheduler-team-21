fee562113569311dd2a9e0ce4106c921948e5c25
Christian Rullan <laptopchris01@gmail.com>
2022-04-05 16:05:15 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
+                ChristianRullan
+                <br></br>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
