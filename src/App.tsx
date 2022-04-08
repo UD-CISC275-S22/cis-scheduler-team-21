92b4092a0c7d9a953a438c7d2d903d2d3ad8b109
Kiefer  Yost <kieferyost@Kiefers-MBP.lan>
2022-04-08 14:56:54 -0400

    Committing the changes to App.tsx in which I added a feature to show/hide the common 4 year plan for the BS degree

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
-import React from "react";
+//import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+
+function addCommonPlanPic(): JSX.Element {
+    return (
+        <img
+            src={
+                "https://i.postimg.cc/CLSS3cqj/Screen-Shot-2022-04-07-at-2-34-26-PM.png"
+            }
+        ></img>
+    );
+}
+interface RevealButtonsProps {
+    setVisible: (newVisibility: boolean) => void;
+}
+
+function RevealButtons({ setVisible }: RevealButtonsProps): JSX.Element {
+    return (
+        <div>
+            <Button onClick={() => setVisible(true)}>
+                Show Common 4 Year Plan
+            </Button>
+            <Button onClick={() => setVisible(false)}>
+                Hide Common 4 Year Plan
+            </Button>
+        </div>
+    );
+}
 
 function App(): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(false);
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <h2>Kiefer</h2>
+            <div>
+                <RevealButtons setVisible={setVisible}></RevealButtons>
+                {visible && (
+                    <img
+                        src={
+                            "https://i.postimg.cc/CLSS3cqj/Screen-Shot-2022-04-07-at-2-34-26-PM.png"
+                        }
+                    ></img>
+                )}
+            </div>
+            <div>{addCommonPlanPic}</div>
+
             <p>
                 ChristianRullan
                 <br></br>
