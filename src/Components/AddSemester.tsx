0721400e78d219c65df562139004b9ed9a65772a
Christian Rullan <laptopchris01@gmail.com>
2022-04-14 15:05:25 -0400

    layering work we have done in buttons

--- src/Components/AddSemester.tsx
+++ src/Components/AddSemester.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { ShowFallSemester } from "../Components/ShowFallSemester";
+import "../App.css";
 
 export function AddSemester(): JSX.Element {
+    const [visible1, setVisible1] = useState<boolean>(false);
     return (
         <Form.Group>
             <br></br>
-            <Button>Add Fall Semester</Button>
+            <div>
+                <Button className="customButton">Add Spring Semester</Button>
+                <Button
+                    className="customButton"
+                    onClick={() => setVisible1(!visible1)}
+                >
+                    Add Fall Semester
+                </Button>
+                <Button className="customButton">Add Winter Semester</Button>
+                <Button className="customButton">Add Summer Semester</Button>
+            </div>
+            <div>
+                <div>{visible1 && <ShowFallSemester></ShowFallSemester>}</div>
+            </div>
         </Form.Group>
     );
 }
