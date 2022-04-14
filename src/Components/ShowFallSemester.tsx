7125e1204b5a4ca2e2f97dd7d3b5e1a11250e89e
Kiefer  Yost <kieferyost@wifi-roaming-128-4-128-203.wifi.udel.edu>
2022-04-14 15:21:49 -0400

    Committing incomplete code from ShowFallSemester

--- src/Components/ShowFallSemester.tsx
+++ src/Components/ShowFallSemester.tsx
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Dropdown, DropdownButton } from "react-bootstrap";
+import { Course } from "../Interfaces/Courses";
+//import { ACCT } from "../Data/catalog.json";
 //import React from "react";
+import Data from "../Data/catalog.json";
+import backupCourseData from "../Data/catalog.json";
 
+/**const { Data }: Record<string, Course> =
+    // Typecast the test data that we imported to be a record matching
+    //  strings to the course list
+    testCourseData as unknown as Record<string, Course[]>;
+
+// We have backup versions of the data to make sure all changes are immutable
+const { ACCT: BACKUP_ACCT_COURSES }: Record<string, Course[]> =
+    backupCourseData as unknown as Record<string, Course[]>;
+*/
+/**const COURSES = Data.map(
+    (course): Course => ({
+        ...course
+    })
+);*/
 export interface AddCourseProps {
     setVisible: (newVisibility: boolean) => void;
 }
+
+const COURSES: Course[] = [];
+
+//COURSES.map((course: Course[]) =>
 
 function AddCourse({ setVisible }: AddCourseProps): JSX.Element {
     return (
@@ -40,6 +62,26 @@ export function ShowFallSemester(): JSX.Element {
                 </Form.Group>
                 //<div className="AddCourse">{course}</div>
             )}
+            <div>
+                <DropdownButton
+                    id="dropdown-basic-button"
+                    title="Dropdown button"
+                >
+                    {COURSES.map((course: Course) => (
+                        <div key={course.code}>
+                            <Dropdown.Item href="#/action-1">
+                                {course.code}
+                            </Dropdown.Item>
+                        </div>
+                    ))}
+                    <Dropdown.Item href="#/action-2">
+                        Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                        Something else
+                    </Dropdown.Item>
+                </DropdownButton>
+            </div>
         </div>
     );
 }
