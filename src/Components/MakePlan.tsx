6b1c7ce06cc6b017c6779a29651f40fb875060dc
Kiefer  Yost <kieferyost@wifi-roaming-128-4-115-217.wifi.udel.edu>
2022-04-27 02:29:44 -0400

    Created a pop up window after the new plan button is clicked

--- src/Components/MakePlan.tsx
+++ src/Components/MakePlan.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Dropdown, Form } from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
 import { Plan } from "../Interfaces/Courses";
 import { PlanContainer } from "./PlanContainer";
 
@@ -8,7 +10,12 @@ import { PlanContainer } from "./PlanContainer";
 export function MakePlan(): JSX.Element {
     const [Plans, setPlans] = useState<Plan[]>([]);
     const [Counter, setCounter] = useState<number>(1);
+    const [Visible, setVisible] = useState<boolean>(false);
+    const [Title, setTitle] = useState<string>("Select Degree");
+    const [Year, setYear] = useState<string>("2022");
+
     function newPlan(): void {
+        setVisible(false);
         const newPlan: Plan = {
             Title: "Plan " + Counter,
             id: Counter
@@ -19,6 +26,61 @@ export function MakePlan(): JSX.Element {
         setPlans(planList);
         setCounter(counterCopy);
     }
+
+    function Popup() {
+        return (
+            <div className="popup">
+                <div className="popup-inner">
+                    <button
+                        className="close-btn"
+                        onClick={() => setVisible(false)}
+                    >
+                        close
+                    </button>
+                    <div>
+                        <Form.Group controlId="formStartYear">
+                            <Form.Control
+                                value={Year}
+                                type="number"
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setYear(event.target.value)}
+                            />
+                        </Form.Group>
+                    </div>
+                    <div>
+                        <Dropdown className="degreedropdown">
+                            <Dropdown.Toggle id="degree" variant="secondary">
+                                {Title}
+                            </Dropdown.Toggle>
+                            <DropdownMenu variant="dark">
+                                <DropdownItem
+                                    onClick={() => setTitle("Bachelor of Art")}
+                                >
+                                    Bachelor of Art
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={() =>
+                                        setTitle("Bachelor of Science")
+                                    }
+                                >
+                                    Bachelor of Science
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                    </div>
+                    <button
+                        className="create-btn"
+                        onClick={newPlan}
+                        disabled={Title == "Select Degree"}
+                    >
+                        Create Plan
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div>
             <div>
@@ -32,7 +94,8 @@ export function MakePlan(): JSX.Element {
                         ></PlanContainer>
                     </div>
                 ))}
-                <Button onClick={newPlan}>New Plan</Button>
+                <Button onClick={() => setVisible(true)}>New Plan</Button>
+                <div className="main">{Visible && <Popup></Popup>}</div>
             </div>
         </div>
     );
