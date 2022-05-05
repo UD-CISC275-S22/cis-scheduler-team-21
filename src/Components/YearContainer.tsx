e262fe59d3fd265d47679c4e80a8792c3e7ab7e4
Christian Rullan <laptopchris01@gmail.com>
2022-05-05 04:03:14 -0400

    add and delete year

--- src/Components/YearContainer.tsx
+++ src/Components/YearContainer.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { setYearProp } from "../Interfaces/yearInterface";
+import { setYearProp, Year } from "../Interfaces/yearInterface";
 import { AddSemester } from "./AddSemester";
 
-export function YearContainer({ year }: setYearProp): JSX.Element {
+export function YearContainer({
+    year,
+    setYear,
+    yearList
+}: setYearProp): JSX.Element {
     const [Visible, setVisible] = useState<boolean>(false);
     const [Semesters] = useState<JSX.Element>(<AddSemester />);
+    function deleteYear(): void {
+        const newYearList: Year[] = yearList.filter(
+            (year1: Year): boolean => year1.id !== year.id
+        );
+        setYear(newYearList);
+    }
     return (
         <div>
             <br></br>
             <span>
-                <span className="yearDisplay">{year}</span>
+                <span className="yearDisplay">{year.title}</span>
                 <Button
                     className="orangeButton"
                     style={{ marginBottom: "4ch", fontSize: "80%" }}
@@ -18,11 +28,21 @@ export function YearContainer({ year }: setYearProp): JSX.Element {
                 >
                     +
                 </Button>
+                <Button
+                    onClick={deleteYear}
+                    style={{
+                        backgroundColor: "darkRed",
+                        marginLeft: "2ch",
+                        fontSize: "1ch",
+                        marginBottom: "5ch"
+                    }}
+                >
+                    Delete
+                </Button>
             </span>
             <div style={{ display: Visible ? "block" : "none" }}>
                 {Semesters}
             </div>
-            <hr></hr>
         </div>
     );
 }
--- src/Components/Years.tsx
+++ src/Components/Years.tsx
@@ -1,31 +1,69 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+//import { useLocation } from "react-router-dom";
 import "../App.css";
 import { YearContainer } from "./YearContainer";
+import { Year } from "../Interfaces/yearInterface";
+import { Button } from "react-bootstrap";
 
-type locationStateString = { yearValue: string };
+//type locationStateString = { yearValue: string };
 
 export function Years(): JSX.Element {
-    const location = useLocation();
-    const { yearValue } = location.state as locationStateString;
-    const yearInt: number = parseInt(yearValue);
-    const yearList: number[] = [
-        yearInt,
-        yearInt + 1,
-        yearInt + 2,
-        yearInt + 3,
-        yearInt + 4
-    ];
+    //const location = useLocation();
+    //const { yearValue } = location.state as locationStateString;
+    //const yearInt: number = parseInt(yearValue);
+    const Freshman: Year = {
+        title: "Freshman",
+        id: 1
+    };
+    const Sophomore: Year = {
+        title: "Sophomore",
+        id: 2
+    };
+    const Junior: Year = {
+        title: "Junior",
+        id: 3
+    };
+    const Senior: Year = {
+        title: "Senior",
+        id: 4
+    };
+    const [yearList, setYearList] = useState<Year[]>([
+        Freshman,
+        Sophomore,
+        Junior,
+        Senior
+    ]);
+    const [counter, setCounter] = useState<number>(5);
+    function addYear(): void {
+        setCounter(counter + 1);
+        const nextCount: number = counter + 1;
+        const newYear: Year = {
+            title: "New School Year",
+            id: nextCount
+        };
+        const yearListCopy: Year[] = [...yearList, newYear];
+        setYearList(yearListCopy);
+    }
     return (
-        <div>
+        <div style={{ paddingBottom: "8ch" }}>
             <header className="App-header-Year">Year View</header>
             {yearList.map(
-                (year: number): JSX.Element => (
-                    <div key={year}>
-                        <YearContainer year={year}></YearContainer>
-                    </div>
+                (year: Year): JSX.Element => (
+                    <span key={year.id}>
+                        <YearContainer
+                            year={year}
+                            setYear={setYearList}
+                            yearList={yearList}
+                        ></YearContainer>
+                        <hr></hr>
+                    </span>
                 )
             )}
+            <div style={{ textAlign: "center" }}>
+                <Button onClick={addYear} className="orangeButton">
+                    Add Year
+                </Button>
+            </div>
         </div>
     );
 }
